88c15e8930d1ef522495d03a60994cb8288be800
mxkxkks1 <ericmgreen2006@gmail.com>
2024-09-04 14:42:25 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,11 +1,24 @@
+--[[
+ __    __     __  __     __  __     __  __     __  __     __  __     ______    
+/\ "-./  \   /\_\_\_\   /\ \/ /    /\ \/ /    /\ \/ /    /\ \/ /    /\  ___\   
+\ \ \-./\ \  \/_/\_\/_  \ \  _"-.  \ \  _"-.  \ \  _"-.  \ \  _"-.  \ \___  \  
+ \ \_\ \ \_\   /\_\/\_\  \ \_\ \_\  \ \_\ \_\  \ \_\ \_\  \ \_\ \_\  \/\_____\ 
+  \/_/  \/_/   \/_/\/_/   \/_/\/_/   \/_/\/_/   \/_/\/_/   \/_/\/_/   \/_____/ 
+                                                                               
+]]
 
 -- Track & Field: Infinite Script made by mxkxkks
 
 -- DO NOT USE WALKSPEED SCRIPTS WHILE USING THIS SCRIPT OR IT WILL GET FLAGGED AS TELEPORTING!
+
 -- Leave the vector values alone if you don't know what you're doing. You can lower the values if you encounter a ban.
+
 -- Please leave everything as it is to avoid kick detection.
+
 -- Script revamped to only touch extender since some kids can't comprehend the main point of the script.
+
 -- Made more visual.
+
 -- Resized checkpoints.
 
 local Players = game:GetService("Players")
@@ -48,17 +61,17 @@ local function adjustHitbox()
 
                 local targetSize -- lower the values if you encounter a ban.
                 if titleText.Text == "300 METER DASH" then
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 300)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 395)
                 elseif titleText.Text == "60 METER DASH" then
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 75)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 70)
                 elseif titleText.Text == "100 METER DASH" then
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 80)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 75)
                 elseif table.find(specificRaces, titleText.Text) then
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 105)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 100)
                 elseif titleText.Text:find("RELAY") then
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 85)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 80)
                 else
-                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 400)
+                    targetSize = Vector3.new(v.Size.X, v.Size.Y, 395)
                 end
 
                 if v.Size ~= targetSize then
